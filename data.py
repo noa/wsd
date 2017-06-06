fcb266e4d868466c8037c66be41f88078dafee09
Nicholas Andrews <noandrews@gmail.com>
2017-06-05 20:46:10 -0400

    sync

--- data.py
+++ data.py
@@ -36,17 +36,25 @@ class BucketedBatchQueue(object):
     record_path = raw_path + '.tfrecords'
     tf.logging.info('Raw input data path: {}'.format(raw_path))
     tf.logging.info('Writing TF example records to: {}'.format(record_path))
+
+    # Step 1: Create training instances from raw IDs file and
+    # serialize in binary TF record format for fast access.
     if not os.path.exists(record_path):
       g = example_generator(raw_path)
       write_records(g, record_path)
+
+    # Step 2: Create an input queue that reads TF records from one (or
+    # more) paths.
     queue = examples_queue(
-      data_sources=record_path,
+      data_sources=record_path, # this is a file pattern (or a single path)
       data_fields_to_features={
         'sequence': tf.VarLenFeature(tf.int64),
         'label': tf.FixedLenFeature([1], tf.int64)
       },
-      training=is_training
+      training=is_training # If is_training==True, the examples are shuffled.
     )
+
+    # Step 3: Read the examples bucketed based on sequence lengths.
     lens, batch = batch_examples(queue, batch_size)
     self._batch = (batch['sequence'], lens, batch['label'])
 
@@ -57,14 +65,19 @@ class BucketedBatchQueue(object):
 class DataTest(tf.test.TestCase):
   def testBucketedProducer(self):
     tmpdatadir = tf.test.get_temp_dir()
+
+    # Step 1: Download data and convert to words to word IDs.
     train_ids_path, dev_ids_path, vocab_path = prepare_ptb_data(
       tmpdatadir,
       space_tokenizer
     )
+
+    # Step 2: Create batch queue
     q = BucketedBatchQueue(train_ids_path, 4, True, 'train_queue')
+
+    # Step 3: Run queue.
     init_op = tf.group(tf.global_variables_initializer(),
                        tf.local_variables_initializer())
-    coord = tf.train.Coordinator()
     with tf.Session() as sess:
       sess.run(init_op)
       with tf.contrib.slim.queues.QueueRunners(sess):
